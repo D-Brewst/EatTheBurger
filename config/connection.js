d9c6d6f51e000a8a515e977f22c20566a14b407e
D-Brewst <devonbrewster31@gmail.com>
2020-08-10 19:16:17 -0400

    fixed package.json

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,42 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// // Set up MySQL connection.
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "KryDevJusNa1200*",
-    database: "burgers_db"
-  });
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "KryDevJusNa1200*",
+//     database: "burgers_db"
+//   });
   
-  // Make connection.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+//   // Make connection.
+//   connection.connect(function(err) {
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+//   });
   
-  // Export connection for our ORM to use.
-  module.exports = connection;
+//   // Export connection for our ORM to use.
+//   module.exports = connection;
+
+// Set up MySQL connection.
+require("dotenv").config();
+var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection(process.env.DB_URL);
+}
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+// Export connection for our ORM to use.
+module.exports = connection;
