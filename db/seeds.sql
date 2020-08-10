5f98de6a7cce271f75c795136fcf25da569f444f
D-Brewst <devonbrewster31@gmail.com>
2020-08-10 17:50:34 -0400

    Inital commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('BigMac');
+INSERT INTO burgers (name) VALUES ('The Whopper');
+INSERT INTO burgers (name) VALUES ('White Castle Silders');
+INSERT INTO burgers (name) VALUES ('In-N-Out Burger');
+INSERT INTO burgers (name) VALUES ('Shake Shack Burger');
+INSERT INTO burgers (name) VALUES ('Smash Burger');
